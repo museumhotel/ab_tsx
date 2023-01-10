f9ac9dcd853f8d9ad7e5074a4aad7805705a2d0f
museumhotel <david@matunda.xyz>
2023-01-10 04:58:33 +0000

    dependency update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c5cd619a0472826a"; }
+/******/ 		__webpack_require__.h = function() { return "77f06d9eda5bad89"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
